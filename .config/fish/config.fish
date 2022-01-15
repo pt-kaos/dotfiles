2859f790fb16c05140a1f8bcc4ab9da68282696b
Pedro <pedro_teodoro@hotmail.com>
2022-01-15 17:12:20 +0000

    First backup

---
+++ .config/fish/config.fish
@@ -0,0 +1,124 @@
+########################################################
+# this fish config file was inspired by the config file
+# shared by:
+########################################################
+#  ____ _____ 
+# |  _ \_   _|  Derek Taylor (DistroTube)
+# | | | || |    http://www.youtube.com/c/DistroTube
+# | |_| || |    http://www.gitlab.com/dwt1/ 
+# |____/ |_|
+########################################################
+# Now it has only what I need and my costumizations
+########################################################
+
+### EXPORT ###
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
+set fish_greeting                      # Supresses fish's intro message
+set TERM "xterm-256color"              # Sets the terminal type
+set EDITOR "nvim"
+
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+### END OF VI MODE ###
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+### FUNCTIONS ###
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ $fish_key_bindings = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+
+### ALIASES ###
+alias nv='nvim'
+
+# Command to manage bare git reposiory for my dotfiles
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# Alias for docker commands
+alias dcr="docker-compose run --rm"
+alias dcf="docker-compose logs -f"
+alias dc="docker-compose"
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+### OS ALIASES ###
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+# confirm before overwriting
+alias rm='rm -i'
+alias mv='mv -i'
+alias cp="cp -i"
+# termbin
+alias tb="nc termbin.com 9999"
+# pacman and yay
+alias pacsyu='sudo pacman -Syyu'                 # update only standard pkgs
+alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs
+alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs
+alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
+# Changing "ls" to "exa"
+alias ll='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ls='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+# navigation
+alias ..='cd ..' 
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+# broot
+#alias br='broot -dhp'
+#alias bs='broot --sizes'
+
+### START NEOFECH ###
+neofetch
+
+### CONDA INITIATIZATION ###
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+#eval /home/pedro/Applications/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
